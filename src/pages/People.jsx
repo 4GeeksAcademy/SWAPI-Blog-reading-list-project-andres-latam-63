50e764f079ae84b8e40cc500e012972ac65c13b7
AEGiner <andresesquivel2011@gmail.com>
2025-12-01 21:15:21 +0000

    changed container size

--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -36,7 +36,7 @@ export const People = () => {
 
   return (
     <>
-      <div className="container  d-flex flex-column justify-content-center mt-5">
+      <div className="container d-flex flex-column justify-content-center mb-0" style={{height:'80vh'}}>
         <div
           className="card mb-3 ms-auto me-auto"
           style={{ maxWidth: "800px" }}
